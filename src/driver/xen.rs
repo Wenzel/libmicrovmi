ec382b83da8af20b83b8f5b3a445b799c1f8f72c
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-05-28 22:57:15 +0200

    use xenctrl crate

--- src/driver/xen.rs
+++ src/driver/xen.rs
@@ -1,15 +1,21 @@
+extern crate xenctrl;
 use crate::api;
+use xenctrl::Xc;
 
 // unit struct
-pub struct Xen;
+pub struct Xen {
+    xc: Xc,
+}
 
 impl api::Introspectable for Xen {
     fn new(&self) {
         println!("Xen driver init !");
+        self.xc = Xc::new();
     }
 
     fn close(&self) {
         println!("Xen driver close !");
+        self.xc::close();
     }
 }
 
