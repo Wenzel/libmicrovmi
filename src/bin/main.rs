b597b1f97b30b819726052f5b84940650deb2c49
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-06-07 23:56:28 +0200

    test pause/resume in main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::thread;
+use std::time;
 extern crate microvmi;
 
 // traits method can only be used if the trait is in the scope
@@ -13,6 +15,16 @@ fn main() {
 
     let drv_type = DriverType::Xen;
     let mut drv: Box<Introspectable> = microvmi::init(drv_type, domid);
+
+    // play with pause and resume
+    println!("pausing the VM");
+    drv.pause();
+    println!("waiting 5 seconds...");
+    let duration = time::Duration::from_millis(5000);
+    thread::sleep(duration);
+    println!("resuming the VM");
+    drv.resume();
+
     // close driver
     drv.close();
 }
