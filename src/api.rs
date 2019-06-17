54940c6b996b1601cd939daa36beecabb6bc5d4f
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-06-17 23:56:40 +0200

    add read_physical API

--- src/api.rs
+++ src/api.rs
@@ -4,6 +4,9 @@ pub enum DriverType {
 }
 
 pub trait Introspectable {
+    // read physical memory
+    fn read_physical(&self, paddr: u64, count: u32) -> Result<Vec<u8>,&str>;
+
     // pause the VM
     fn pause(&self);
 
