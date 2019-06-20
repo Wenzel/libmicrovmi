47d8f1a56dd59ad0af7d8f643acf66aefb87f9c4
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-06-20 23:41:42 +0200

    mem-dump: dump content to a file

--- src/bin/mem-dump.rs
+++ src/bin/mem-dump.rs
@@ -1,4 +1,7 @@
 use std::env;
+use std::path::Path;
+use std::fs::File;
+use std::io::Write;
 extern crate microvmi;
 
 // traits method can only be used if the trait is in the scope
@@ -14,6 +17,9 @@ fn main() {
         return;
     }
     let domain_name = &args[1];
+    let dump_name = format!("{}.dump", domain_name);
+    let dump_path = Path::new(&dump_name);
+    let mut dump_file = File::create(dump_path).expect("Fail to open dump file");
 
     let drv_type = DriverType::Xen;
     let drv: Box<Introspectable> = microvmi::init(drv_type, domain_name);
@@ -25,11 +31,15 @@ fn main() {
     let mut cur_addr: u64 = 0;
     let max_addr = drv.get_max_physical_addr().unwrap();
     println!("Max address @{:x}", max_addr);
+    println!("Dumping physical memory to {}", dump_path.display());
     while cur_addr < max_addr {
         let result = drv.read_physical(cur_addr, &mut buffer);
         match result {
-            Ok(()) => println!("page read success 0x{:x}", cur_addr),
-            Err(error) => println!("page read failed 0x{:x}", cur_addr),
+            Ok(()) => {
+                dump_file.write(&buffer).expect("failed to write to file");
+                ()
+            },
+            Err(_error) => (),
         }
         cur_addr += PAGE_SIZE as u64;
     }
