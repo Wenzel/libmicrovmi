9700fc0da70808b5c56bcfa6b95092dfe389ea77
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-06-20 23:18:58 +0200

    test binary becomes mem-dump

--- src/bin/mem-dump.rs
+++ src/bin/mem-dump.rs
@@ -1,11 +1,11 @@
 use std::env;
-use std::thread;
-use std::time;
 extern crate microvmi;
 
 // traits method can only be used if the trait is in the scope
 use microvmi::api::Introspectable;
 use microvmi::api::DriverType;
+
+const PAGE_SIZE: usize = 4096;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,13 +18,22 @@ fn main() {
     let drv_type = DriverType::Xen;
     let drv: Box<Introspectable> = microvmi::init(drv_type, domain_name);
 
-    // play with pause and resume
     println!("pausing the VM");
     drv.pause();
-    println!("waiting 5 seconds...");
-    let duration = time::Duration::from_millis(5000);
-    thread::sleep(duration);
+
+    let mut buffer: [u8; PAGE_SIZE] = [0; PAGE_SIZE];
+    let mut cur_addr: u64 = 0;
+    let max_addr = drv.get_max_physical_addr().unwrap();
+    println!("Max address @{:x}", max_addr);
+    while cur_addr < max_addr {
+        let result = drv.read_physical(cur_addr, &mut buffer);
+        match result {
+            Ok(()) => println!("page read success 0x{:x}", cur_addr),
+            Err(error) => println!("page read failed 0x{:x}", cur_addr),
+        }
+        cur_addr += PAGE_SIZE as u64;
+    }
+
     println!("resuming the VM");
     drv.resume();
-
 }
