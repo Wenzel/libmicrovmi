fb88db5f088875903a428d4bed6836c3c1728dd2
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-07-26 00:21:19 +0200

    dummy: implement new error handling

--- src/driver/dummy.rs
+++ src/driver/dummy.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use crate::api;
 
 // unit struct
@@ -13,21 +14,23 @@ impl Dummy {
 }
 
 impl api::Introspectable for Dummy {
-    fn read_physical(&self, paddr: u64, buf: &mut [u8]) -> Result<(),&str> {
+    fn read_physical(&self, paddr: u64, buf: &mut [u8]) -> Result<(),Box<Error>> {
         println!("dummy read physical - @{}, {:#?}", paddr, buf);
         Ok(())
     }
 
-    fn get_max_physical_addr(&self) -> Result<u64,&str> {
+    fn get_max_physical_addr(&self) -> Result<u64,Box<Error>> {
         println!("dummy get max physical address");
         Ok(0)
     }
 
-    fn pause(&mut self) {
+    fn pause(&mut self) -> Result<(),Box<Error>> {
         println!("dummy pause");
+        Ok(())
     }
 
-    fn resume(&mut self) {
+    fn resume(&mut self) -> Result<(),Box<Error>> {
         println!("dummy resume");
+        Ok(())
     }
 }
