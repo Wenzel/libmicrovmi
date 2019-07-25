3078019200d9657693b7209295b4686376144f0a
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-07-25 21:29:58 +0200

    remove useless brackets

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@ mod driver;
 
 use api::Introspectable;
 use api::DriverType;
-use driver::dummy::{Dummy};
+use driver::dummy::Dummy;
 #[cfg(feature="xen")]
-use driver::xen::{Xen};
+use driver::xen::Xen;
 #[cfg(feature="kvm")]
-use driver::kvm::{Kvm};
+use driver::kvm::Kvm;
 
 pub fn init(driver_type: DriverType, domain_name: &String) -> Box<Introspectable> {
     println!("vmi init");
