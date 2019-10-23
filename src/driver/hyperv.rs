2e282195df939a3fb6b551d75814caab5f14d1e4
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-10-23 16:44:01 +0200

    hyperv: use iterator to enumerate processes from snapshot

--- src/driver/hyperv.rs
+++ src/driver/hyperv.rs
@@ -1,60 +1,91 @@
 use std::error::Error;
 use std::mem;
 use std::convert::TryInto;
+use std::iter::Iterator;
 
 use crate::api;
 use winapi::um::tlhelp32::{CreateToolhelp32Snapshot, TH32CS_SNAPPROCESS, Process32FirstW, Process32NextW, PROCESSENTRY32W};
 use winapi::um::winnt::HANDLE;
 use winapi::um::handleapi::INVALID_HANDLE_VALUE;
-use winapi::shared::minwindef::{TRUE, FALSE};
+use winapi::shared::minwindef::{TRUE, FALSE, BOOL, DWORD};
 use widestring::U16CString;
+
+// iterator over processes
+struct ProcessList {
+    snapshot_handle: HANDLE,
+    // whether we retrieved the first process already
+    first_process_done: bool,
+    proc_entry: PROCESSENTRY32W,
+}
+
+impl ProcessList {
+    fn new() -> Self {
+        let snapshot_handle = unsafe {
+            CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)
+        };
+        println!("CreateToolHelp32Snapshot handle: {:?}", snapshot_handle);
+        if snapshot_handle == INVALID_HANDLE_VALUE {
+            panic!("CreateToolHelp32Snapshot failed !");
+        }
+        let mut list = ProcessList {
+            snapshot_handle,
+            first_process_done: false,
+            proc_entry: unsafe {
+                mem::MaybeUninit::<PROCESSENTRY32W>::zeroed().assume_init()
+            },
+        };
+        // must set dwSize
+        list.proc_entry.dwSize = mem::size_of::<PROCESSENTRY32W>().try_into().unwrap();
+        list
+    }
+}
+
+impl Iterator for ProcessList {
+    type Item = PROCESSENTRY32W;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let res: BOOL = match self.first_process_done {
+            false => {
+                match unsafe { Process32FirstW(self.snapshot_handle, &mut self.proc_entry) } {
+                    TRUE => {
+                        self.first_process_done = true;
+                        TRUE
+                    },
+                    _ => panic!("Process32FirstW failed !")
+                }
+            },
+            true => {
+                unsafe { Process32NextW(self.snapshot_handle, &mut self.proc_entry) }
+            }
+        };
+        match res {
+            FALSE => None,
+            TRUE => Some(self.proc_entry),
+            _ => panic!("Unexpected return value from Process32NextW"),
+        }
+    }
+}
 
 // unit struct
 #[derive(Debug)]
-pub struct HyperV {
-	a: i32
-}
+pub struct HyperV;
 
 impl HyperV {
 
     pub fn new(domain_name: &str) -> Self {
         println!("HyperV driver init on {}", domain_name);
-        // snapshot
-        let snapshot_handle: HANDLE = unsafe {
-            CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)
-        };
-        println!("snapshot handle: {:?}", snapshot_handle);
-        if snapshot_handle == INVALID_HANDLE_VALUE {
-            panic!("CreateToolHelp32Snapshot failed !");
-        }
-
-        // set size of proc_entry before getting first process from snapshot
-        let mut proc_entry: PROCESSENTRY32W = unsafe {
-            mem::MaybeUninit::<PROCESSENTRY32W>::zeroed().assume_init()
-        };
-        proc_entry.dwSize = mem::size_of::<PROCESSENTRY32W>().try_into().unwrap();
-        
-        // get first process
-        let res = unsafe {
-            Process32FirstW(snapshot_handle, &mut proc_entry)
-        };
-        if res != TRUE {
-            panic!("Process32FirstW failed !");
-        }
-
         let vmwp_name: U16CString = U16CString::from_str("vmwp.exe").unwrap();
-        while unsafe { Process32NextW(snapshot_handle, &mut proc_entry) } != FALSE
-        {
-            // find vmwp.exe
+
+        let process_list = ProcessList::new();
+        for proc_entry in process_list {
             let exe_file = unsafe { U16CString::from_ptr_str(&proc_entry.szExeFile as *const u16) };
+            let pid: DWORD = proc_entry.th32ProcessID;
             if exe_file == vmwp_name {
-                println!("found a VM process !");
+                println!("Found Hyper-V VM process - PID: {}", pid);
             }
         }
 
-        let hyperv = HyperV {
-			a: 0
-        };
+        let hyperv = HyperV;
         hyperv
     }
 
