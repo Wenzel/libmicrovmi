f1e7eb3d03de2fe25e3656ef01b9a352d9d94125
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-10-30 00:56:15 +0100

    kvm: read registers

--- src/driver/kvm.rs
+++ src/driver/kvm.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use crate::api;
+use crate::api::{Introspectable, Registers, X86Registers};
 use kvmi::{KVMi, KVMiEventType};
 
 // unit struct
@@ -25,7 +25,7 @@ impl Kvm {
     }
 }
 
-impl api::Introspectable for Kvm {
+impl Introspectable for Kvm {
 
     fn read_physical(&self, paddr: u64, buf: &mut [u8]) -> Result<(),Box<dyn Error>> {
         Ok(self.kvmi.read_physical(paddr, buf)?)
@@ -36,6 +36,31 @@ impl api::Introspectable for Kvm {
         // fake 512MB
         let max_addr = 1024 * 1024 * 512;
         Ok(max_addr)
+    }
+
+    fn read_registers(&self, vcpu: u16) -> Result<Registers, Box<dyn Error>> {
+        let (regs, _sregs, _msrs) = self.kvmi.get_registers(vcpu)?;
+        // TODO: hardcoded for x86 for now
+        Ok(Registers::X86(X86Registers {
+            rax: regs.rax,
+            rbx: regs.rbx,
+            rcx: regs.rcx,
+            rdx: regs.rdx,
+            rsi: regs.rsi,
+            rdi: regs.rdi,
+            rsp: regs.rsp,
+            rbp: regs.rbp,
+            r8:  regs.r8,
+            r9:  regs.r9,
+            r10: regs.r10,
+            r11: regs.r11,
+            r12: regs.r12,
+            r13: regs.r13,
+            r14: regs.r14,
+            r15: regs.r15,
+            rip: regs.rip,
+            rflags: regs.rflags,
+        }))
     }
 
     fn pause(&mut self) -> Result<(),Box<dyn Error>> {
