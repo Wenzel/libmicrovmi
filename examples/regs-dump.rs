eb3da6db6aae5f717f73490ccb2eefb6330648f6
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-11-03 17:40:12 +0100

    example: use env_logger in regs-dump

--- examples/regs-dump.rs
+++ examples/regs-dump.rs
@@ -1,10 +1,15 @@
 use std::env;
 extern crate microvmi;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
 // traits method can only be used if the trait is in the scope
 use microvmi::api::{Introspectable, Registers};
 
 fn main() {
+    env_logger::init();
+
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         println!("Usage: {} <vm_name>", args[0]);
