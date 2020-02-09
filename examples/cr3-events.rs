1f6e9821f54718ab18629cfd023977008ecc2689
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-02-09 19:05:27 +0100

    examples: add ctrl handling in cr3-events

--- examples/cr3-events.rs
+++ examples/cr3-events.rs
@@ -1,8 +1,11 @@
 use std::env;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::time::Instant;
 
 use env_logger;
 
-use microvmi::api::{CrType, Event, EventReplyType, EventType, Introspectable};
+use microvmi::api::{CrType, EventType, InterceptType, Introspectable};
 
 fn main() {
     env_logger::init();
@@ -14,33 +17,51 @@ fn main() {
     }
     let domain_name = &args[1];
 
+    // set CTRL-C handler
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
     let mut drv: Box<dyn Introspectable> = microvmi::init(domain_name, None);
 
     println!("Enable CR3 interception");
     // enable CR3 interception
-    let empty_cr_enum = EventType::Cr {
-        cr_type: CrType::Cr3,
-        new: 0,
-        old: 0,
-    };
-    drv.toggle_intercept(0, empty_cr_enum, true)
+    let inter_cr3 = InterceptType::Cr(CrType::Cr3);
+    drv.toggle_intercept(0, inter_cr3, true)
         .expect("Failed to enable CR3 interception");
 
     println!("Listen for CR3 events...");
+    // record elapsed time
+    let start = Instant::now();
     // listen
-    for i in 1..100 {
+    let mut i: u64 = 1;
+    while running.load(Ordering::SeqCst) {
         let event = drv.listen(1000).expect("Failed to listen for events");
         match event {
             Some(ev) => {
-                println!("[{}] {:?}", i, ev);
-                drv.reply_event(ev, EventReplyType::Continue);
+                let new = match ev.kind {
+                    EventType::Cr {
+                        cr_type: _,
+                        new,
+                        old: _,
+                    } => new,
+                };
+                println!("[{}] CR3: 0x{:x}", i, new);
+                // ev.reply(EventReplyType::Continue);
             }
             None => println!("No events yet..."),
         }
+        i = i + 1;
     }
+    let duration = start.elapsed();
 
     println!("Disable CR3 interception");
     // disable CR3 interception
-    drv.toggle_intercept(0, empty_cr_enum, true)
+    drv.toggle_intercept(0, inter_cr3, false)
         .expect("Failed to enable CR3 interception");
+
+    println!("Catched {} events/sec", i / duration.as_secs());
 }
