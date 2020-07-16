fd658f404b6aed86b7bb6cc8d643f7ea4540ffb4
arnabcs17b006 <cs17b006@iittp.ac.in>
2020-07-16 11:48:33 +0530

    reading and writing registers

--- src/driver/xen.rs
+++ src/driver/xen.rs
@@ -2,6 +2,7 @@ use crate::api::{
     DriverInitParam, Introspectable, Registers, SegmentReg, SystemTableReg, X86Registers,
 };
 use libc::{PROT_READ, PROT_WRITE};
+use std::convert::TryInto;
 use std::error::Error;
 use std::io::ErrorKind;
 use xenctrl::consts::{PAGE_SHIFT, PAGE_SIZE};
@@ -139,6 +140,11 @@ impl Introspectable for Xen {
         Ok(max_gpfn << PAGE_SHIFT)
     }
 
+    fn get_vcpu_count(&self) -> Result<u16, Box<dyn Error>> {
+        let domain_info = self.xc.domain_getinfo(self.domid)?;
+        Ok((domain_info.max_vcpu_id + 1).try_into()?)
+    }
+
     fn read_registers(&self, vcpu: u16) -> Result<Registers, Box<dyn Error>> {
         let hvm_cpu = self.xc.domain_hvm_getcontext_partial(self.domid, vcpu)?;
         // TODO: hardcoded for x86 for now
@@ -164,54 +170,47 @@ impl Introspectable for Xen {
             cr0: hvm_cpu.cr0,
             cr3: hvm_cpu.cr3,
             cr4: hvm_cpu.cr4,
-            cr2: 0,
-            sysenter_cs: 0,
-            sysenter_esp: 0,
-            sysenter_eip: 0,
-            msr_efer: 0,
-            msr_star: 0,
-            msr_lstar: 0,
-            efer: 0,
-            apic_base: 0,
+            cr2: hvm_cpu.cr2,
+            sysenter_cs: hvm_cpu.sysenter_cs,
+            sysenter_esp: hvm_cpu.sysenter_esp,
+            sysenter_eip: hvm_cpu.sysenter_eip,
+            msr_efer: hvm_cpu.msr_efer,
+            msr_star: hvm_cpu.msr_star,
+            msr_lstar: hvm_cpu.msr_lstar,
             cs: SegmentReg {
-                base: 0,
-                limit: 0,
-                selector: 0,
+                base: hvm_cpu.cs_base,
+                limit: hvm_cpu.cs_limit,
+                selector: hvm_cpu.cs_sel.try_into().unwrap(),
             },
             ds: SegmentReg {
-                base: 0,
-                limit: 0,
-                selector: 0,
+                base: hvm_cpu.ds_base,
+                limit: hvm_cpu.ds_limit,
+                selector: hvm_cpu.ds_sel.try_into().unwrap(),
             },
             es: SegmentReg {
-                base: 0,
-                limit: 0,
-                selector: 0,
+                base: hvm_cpu.es_base,
+                limit: hvm_cpu.es_limit,
+                selector: hvm_cpu.es_sel.try_into().unwrap(),
             },
             fs: SegmentReg {
-                base: 0,
-                limit: 0,
-                selector: 0,
+                base: hvm_cpu.fs_base,
+                limit: hvm_cpu.fs_limit,
+                selector: hvm_cpu.fs_sel.try_into().unwrap(),
             },
             gs: SegmentReg {
-                base: 0,
-                limit: 0,
-                selector: 0,
+                base: hvm_cpu.gs_base,
+                limit: hvm_cpu.gs_limit,
+                selector: hvm_cpu.gs_sel.try_into().unwrap(),
             },
             ss: SegmentReg {
-                base: 0,
-                limit: 0,
-                selector: 0,
+                base: hvm_cpu.ss_base,
+                limit: hvm_cpu.ss_limit,
+                selector: hvm_cpu.ss_sel.try_into().unwrap(),
             },
             tr: SegmentReg {
-                base: 0,
-                limit: 0,
-                selector: 0,
-            },
-            ldt: SegmentReg {
-                base: 0,
-                limit: 0,
-                selector: 0,
+                base: hvm_cpu.tr_base,
+                limit: hvm_cpu.tr_limit,
+                selector: hvm_cpu.tr_sel.try_into().unwrap(),
             },
             idt: SystemTableReg {
                 base: hvm_cpu.idtr_base,
@@ -221,7 +220,68 @@ impl Introspectable for Xen {
                 base: hvm_cpu.gdtr_base,
                 limit: hvm_cpu.gdtr_limit as u16,
             },
+            ..Default::default()
         }))
+    }
+
+    fn write_registers(&self, vcpu: u16, reg: Registers) -> Result<(), Box<dyn Error>> {
+        let (buffer, mut cpu, size) = self.xc.domain_hvm_getcontext(self.domid, vcpu)?;
+        match reg {
+            Registers::X86(x86_registers) => {
+                cpu.rax = x86_registers.rax;
+                cpu.rbx = x86_registers.rbx;
+                cpu.rcx = x86_registers.rcx;
+                cpu.rdx = x86_registers.rdx;
+                cpu.rsi = x86_registers.rsi;
+                cpu.rdi = x86_registers.rdi;
+                cpu.rsp = x86_registers.rsp;
+                cpu.rbp = x86_registers.rbp;
+                cpu.r8 = x86_registers.r8;
+                cpu.r9 = x86_registers.r9;
+                cpu.r10 = x86_registers.r10;
+                cpu.r11 = x86_registers.r11;
+                cpu.r12 = x86_registers.r12;
+                cpu.r13 = x86_registers.r13;
+                cpu.r14 = x86_registers.r14;
+                cpu.r15 = x86_registers.r15;
+                cpu.rip = x86_registers.rip;
+                cpu.rflags = x86_registers.rflags;
+                cpu.cr0 = x86_registers.cr0;
+                cpu.cr2 = x86_registers.cr2;
+                cpu.cr3 = x86_registers.cr3;
+                cpu.cr4 = x86_registers.cr4;
+                cpu.sysenter_cs = x86_registers.sysenter_cs;
+                cpu.sysenter_esp = x86_registers.sysenter_esp;
+                cpu.sysenter_eip = x86_registers.sysenter_eip;
+                cpu.msr_star = x86_registers.msr_star;
+                cpu.msr_lstar = x86_registers.msr_lstar;
+                cpu.msr_efer = x86_registers.msr_efer;
+                cpu.cs_base = x86_registers.cs.base;
+                cpu.ds_base = x86_registers.ds.base;
+                cpu.es_base = x86_registers.es.base;
+                cpu.fs_base = x86_registers.fs.base;
+                cpu.gs_base = x86_registers.gs.base;
+                cpu.ss_base = x86_registers.ss.base;
+                cpu.tr_base = x86_registers.tr.base;
+                cpu.cs_limit = x86_registers.cs.limit;
+                cpu.ds_limit = x86_registers.ds.limit;
+                cpu.es_limit = x86_registers.es.limit;
+                cpu.fs_limit = x86_registers.fs.limit;
+                cpu.gs_limit = x86_registers.gs.limit;
+                cpu.ss_limit = x86_registers.ss.limit;
+                cpu.tr_limit = x86_registers.tr.limit;
+                cpu.cs_sel = x86_registers.cs.selector.try_into().unwrap();
+                cpu.ds_sel = x86_registers.ds.selector.try_into().unwrap();
+                cpu.es_sel = x86_registers.es.selector.try_into().unwrap();
+                cpu.fs_sel = x86_registers.fs.selector.try_into().unwrap();
+                cpu.gs_sel = x86_registers.gs.selector.try_into().unwrap();
+                cpu.ss_sel = x86_registers.ss.selector.try_into().unwrap();
+                cpu.tr_sel = x86_registers.tr.selector.try_into().unwrap();
+            }
+        }
+        self.xc
+            .domain_hvm_setcontext(self.domid, buffer, size.try_into().unwrap())?;
+        Ok(())
     }
 
     fn pause(&mut self) -> Result<(), Box<dyn Error>> {
