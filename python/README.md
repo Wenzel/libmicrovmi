e4386dc1255d4ea877e5df470640a9f1cd0487b4
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-02-03 16:48:39 +0100

    python: add README and metadata

---
+++ python/README.md
@@ -0,0 +1,43 @@
+# Microvmi Python bindings
+
+> Python bindings for libmicrovmi
+
+## Requirements
+
+- `Python 3`
+
+## Installation
+
+~~~
+virtualenv -p python3 venv
+source venv/bin/actvate
+(venv) python -p pip install -r requirements.txt
+(venv) ./setup.py install
+~~~
+
+## Usage
+
+### Initializing libmicrovmi:
+
+~~~Python
+from microvmi import Microvmi
+
+micro = Microvmi("Windows10")
+~~~
+
+### Specifying the hypervisor:
+
+~~~Python
+from microvmi import Microvmi, DriverType
+
+micro = Microvmi("Windows10", DriverType.XEN)
+~~~
+
+### Adding driver initialization parameters
+
+~~~Python
+from microvmi import Microvmi, DriverType, DriverInitParam
+
+init_param = DriverInitParam.kvmi_unix_socket("/tmp/introspector")
+micro = Microvmi("Windows10", DriverType.KVM, init_param)
+~~~
--- python/setup.py
+++ python/setup.py
@@ -2,11 +2,30 @@
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="microvmi",
     version="0.0.1",
+    author="Mathieu Tarral",
+    author_email="mathieu.tarral@protonmail.com",
+    description="Python bindings for libmicrovmi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Wenzel/libmicrovmi",
+    install_requires=requirements,
     rust_extensions=[RustExtension("microvmi.pymicrovmi", binding=Binding.PyO3)],
     packages=["microvmi"],
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    python_requires=">=3",
 )
