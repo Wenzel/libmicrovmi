4f2c949991a24b63c3db0f84f3a11c50dff75038
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-06-21 13:33:02 +0200

    params: add PartialEq

--- src/api/params.rs
+++ src/api/params.rs
@@ -1,30 +1,30 @@
 /// This module describes initialization parameters for all libmicrovmi drivers
 
 /// Xen initialization parameters
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum XenInitParams {}
 
 /// KVM initialization parameters
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum KVMInitParams {
     UnixSocket { path: String },
 }
 
 /// VirtualBox initialization parameters
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum VBoxInitParams {}
 
 /// Common initialization parameters
 ///
 /// These parameters are shared by two or more drivers, and are stored in this struct
 /// to avoid duplication and simplify the API
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct CommonInitParams {
     pub vm_name: String,
 }
 
 /// This struct is used to specify the initialization parameters for all drivers
-#[derive(Default, Debug, Clone)]
+#[derive(Default, Debug, Clone, PartialEq)]
 pub struct DriverInitParams {
     pub common: Option<CommonInitParams>,
     pub xen: Option<XenInitParams>,
