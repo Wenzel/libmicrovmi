62cc869bf5926ef8196a475ab8d827312c3e350e
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-06-29 15:28:38 +0200

    memflow: impl read_physical

--- src/driver/memflow.rs
+++ src/driver/memflow.rs
@@ -2,7 +2,9 @@ use crate::api::params::{DriverInitParams, MemflowConnectorParams};
 use crate::api::{DriverType, Introspectable};
 use std::error::Error;
 
-use memflow::connector::{ConnectorArgs, ConnectorInventory};
+use memflow::connector::{ConnectorArgs, ConnectorInstance, ConnectorInventory};
+use memflow::{PhysicalAddress, PhysicalMemory};
+use std::cell::RefCell;
 
 #[derive(thiserror::Error, Debug)]
 pub enum MemflowDriverError {
@@ -12,7 +14,11 @@ pub enum MemflowDriverError {
     InvalidConnectorArgument(String),
 }
 
-pub struct Memflow;
+pub struct Memflow {
+    // refcell required because read methods are mutable
+    // contrary to our read_frame signature
+    connector: RefCell<ConnectorInstance>,
+}
 
 impl Memflow {
     pub fn new(init_params: DriverInitParams) -> Result<Self, Box<dyn Error>> {
@@ -42,15 +48,30 @@ impl Memflow {
             "Memflow: create connector - name: {}, args: {:#?}",
             &memflow_init_params.connector_name, &create_connector_args
         );
-        unsafe {
+        let connector = unsafe {
             inventory
                 .create_connector(&memflow_init_params.connector_name, &create_connector_args)?
         };
-        Ok(Memflow {})
+        Ok(Memflow {
+            connector: RefCell::new(connector),
+        })
     }
 }
 
 impl Introspectable for Memflow {
+    fn read_physical(
+        &self,
+        paddr: u64,
+        buf: &mut [u8],
+        bytes_read: &mut u64,
+    ) -> Result<(), Box<dyn Error>> {
+        self.connector
+            .borrow_mut()
+            .phys_read_into(PhysicalAddress::from(paddr), buf)?;
+        *bytes_read = buf.len() as u64;
+        Ok(())
+    }
+
     fn get_driver_type(&self) -> DriverType {
         DriverType::Memflow
     }
