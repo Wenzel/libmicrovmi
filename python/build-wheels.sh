b38dd6c3a6d926a2bbf0a897b2318869e0287499
Mathieu Tarral <mathieu.tarral@protonmail.com>
2025-11-30 14:36:18 +0100

    python/build_wheels.sh: add support for Python3.13 and 3.14

--- python/build-wheels.sh
+++ python/build-wheels.sh
@@ -16,7 +16,7 @@ export BINDGEN_EXTRA_CLANG_ARGS="-I/opt/rh/devtoolset-10/root/usr/lib/gcc/x86_64
 # map libmicrovmi root dir to /io
 cd /io/python
 
-for PYBIN in /opt/python/cp{39,310,311,312}*/bin; do
+for PYBIN in /opt/python/cp{39,310,311,312,313,314}*/bin; do
     "${PYBIN}/pip" install -r requirements.txt
     "${PYBIN}/python" setup.py bdist_wheel $@
 done
